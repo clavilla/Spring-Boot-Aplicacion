8e2f2366caf4c739b440f421eb04fad410681f30
Cristian Lavilla <cristianlavilla@hotmail.com>
2020-11-19 15:23:26 -0300

    Commit inicial con STS

---
+++ Aplicacion/src/test/java/com/grupo1/springApp/AplicacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo1.springApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
